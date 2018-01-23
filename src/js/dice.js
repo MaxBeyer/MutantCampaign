e64ad85a264393b232ca4552bc4f0deb12021347
Max Beyer <inflatableanchor@gmail.com>
2018-01-23 16:33:54 -0600

    removing hard coded value

--- src/js/dice.js
+++ src/js/dice.js
@@ -30,7 +30,7 @@ rollMutationButton.onclick = function() {
   let result2 = dice.roll();
   let baseSixResult = (result1 * 10) + result2;
   printNumber(result1, result2);
-  printMutation(11);
+  printMutation(baseSixResult);
 };
 
 let acidSpit = {
